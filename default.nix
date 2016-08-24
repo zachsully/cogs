cebf9b679f3efe6965ac2ddc6b2790a6bc1eaac2
Zach Sullivan <zsulliva@indiana.edu>
2016-08-24 11:06:35 -0400

    cogs project init

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, stdenv }:
+      mkDerivation {
+        pname = "cogs";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ base ];
+        homepage = "zachsully.com/cogs";
+        description = "A language for numeric and symbolic computing";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
