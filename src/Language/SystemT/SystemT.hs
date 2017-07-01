1d570671a75d41b99e87bb1923c699e70e55be50
Zach Sullivan <zachsully@gmail.com>
2017-06-30 21:32:50 -0700

    add system t interpreter

---
+++ src/Language/SystemT/SystemT.hs
@@ -0,0 +1,83 @@
+module Language.SystemT where
+
+import Prelude hiding (pred,sum)
+
+data Expr
+  = Var String
+  | Zero
+  | Succ Expr
+  | Lam String Expr
+  | App Expr Expr
+  | Rec Expr Expr Expr
+  deriving Show
+
+data Val
+  = Nat Expr
+  | Closure String Expr Env
+  deriving Show
+
+prettyVal :: Val -> String
+prettyVal (Nat x) = show $ go x
+  where go Zero     = 0
+        go (Succ z) = 1 + (go z)
+
+data Env = Env [(String,Expr,Env)]
+  deriving Show
+
+lookupEnv s (Env []) = error $ "unbound var: " ++ s
+lookupEnv s (Env ((s',x,e'):rest)) = case s == s' of
+                                       True -> (x,e')
+                                       False -> lookupEnv s (Env rest)
+
+extendEnv s x e (Env e') = Env ((s,x,e):e')
+
+--------------------------------------------------------------------------------
+--                                EVALUATION                                  --
+--------------------------------------------------------------------------------
+
+evalClosedTerm = eval (Env [])
+
+eval :: Env -> Expr -> Val
+eval e (Var s)        = case lookupEnv s e of
+                          (x,e') -> eval e' x
+eval _ Zero           = Nat Zero
+
+eval e (Succ x)       = case eval e x of
+                          Nat x' -> Nat (Succ x')
+                          _ -> error "type error"
+
+eval e (Lam s x)      = Closure s x e
+
+eval e (App x1 x2)    = case eval e x1 of
+                          Closure s' x1' e1' -> eval (extendEnv s' x1' e1' e) x2
+                          _ -> error "type error"
+
+eval e (Rec x c1 c2)  = case eval e x of
+                          Nat Zero -> eval e c1
+                          Nat (Succ x') ->
+                            case c2 of
+                              (Lam a (Lam b t)) ->
+                                let e'  = extendEnv a x' e e
+                                    x'' = (Rec x' c1 c2)
+                                    e'' = extendEnv b x'' e' e'
+                                in eval e'' t
+                              _ -> error "type error"
+                          _ -> error "type error"
+
+
+--------------------------------------------------------------------------------
+--                                Programs                                    --
+--------------------------------------------------------------------------------
+
+foo1 = Rec (Succ (Succ (Succ (Succ (Succ Zero)))))
+           Zero
+           (Lam "x" (Lam "r" (Var "x")))
+
+foo2 = Rec (Succ (Succ (Succ (Succ (Succ Zero)))))
+           Zero
+           (Lam "x" (Lam "r" (Var "r")))
+
+-- MACROS
+nat x = foldr (\s p -> s p) Zero (replicate x Succ)
+pred x = Rec x Zero (Lam "x" (Lam "r" (Var "x")))
+sum x y = Rec x y (Lam "x" (Lam "r" (Succ (Var "r"))))
