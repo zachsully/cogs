ff708b02223fe950b5d12fa355686111321a10ff
Zach Sullivan <zachsully@gmail.com>
2017-07-09 13:01:06 -0700

    mnore work on compilation a la carte

--- src/Cogs/Language/STLC/Syntax.hs
+++ src/Cogs/Language/STLC/Syntax.hs
@@ -8,33 +8,41 @@ module Cogs.Language.STLC.Syntax where
 import Data.Text
 import Data.Monoid
 
-data Fix f =  In (f (Fix f))
+data Mu f =  In (f (Mu f))
+
+fold :: Functor f => (f a -> a) -> Mu f -> a
+fold f (In t) = f (fmap (fold f) t)
+
+
 data Value m where
   Nat     :: Int -> Value m
   Closure :: Monad m => [Value m] -> m (Value m) -> Value m
 
+-- call-by-value
 class Monad m => CBVMonad m where
-  env  :: m [Value m]
-  with :: [Value m] -> m (Value m) -> m (Value m)
+  envCBV  :: m [Value m]
+  withCBV :: [Value m] -> m (Value m) -> m (Value m)
 
-fold :: Functor t => (t a -> a) -> Fix t -> a
-fold f (In t) = f (fmap (fold f) t)
+-- call-by-name
+class Monad m => CBNMonad m where
+  envCBN  :: m [m (Value m)]
+  withCBN :: [m (Value m)] -> m (Value m) -> m (Value m)
 
-eval :: Eval t m => Fix t -> m (Value m)
-eval = undefined -- fold evalAlgebra
+eval :: Eval t m => Mu t -> m (Value m)
+eval = fold evalAlgebra
 
-pretty :: Pretty t => Fix t -> Text
+pretty :: Pretty t => Mu t -> Text
 pretty (In t) = prettyAlgebra t
 
 --------------------------------------------------------------------------------
 --                                ALGEBRAS                                    --
 --------------------------------------------------------------------------------
 
-class (Functor t, Monad m) => Eval t m where
-  evalAlgebra :: t (m Int) -> m (Value m)
+class (Functor f, Monad m) => Eval f m where
+  evalAlgebra :: f (m (Value m)) -> m (Value m)
 
 class Functor t => Pretty t where
-  prettyAlgebra :: Pretty t' => t (Fix t') -> Text
+  prettyAlgebra :: Pretty t' => t (Mu t') -> Text
 
 --------------------------------------------------------------------------------
 --                              CONSTRUCTORS                                  --
@@ -73,15 +81,25 @@ instance Functor Lambda where
   fmap f (App t1 t2) = App (f t1) (f t2)
 
 instance CBVMonad m => Eval Lambda m where
-  evalAlgebra (Lam t) = undefined --env >>= \e -> return (Closure e i)
+  evalAlgebra (Lam t)     = envCBV >>= \e -> return (Closure e t)
+  evalAlgebra (Index i)   = envCBV >>= \e -> return (e !! i)
+  evalAlgebra (App t1 t2) = do { (Closure e t1') <- t1
+                               ; t2' <- t2
+                               ; withCBV (t2':e) t1' }
 
--- instance Pretty Lambda where
---   prettyAlgebra (Lam (In t)) = "Lam (" <> prettyAlgebra <> ")"
+instance Pretty Lambda where
+  prettyAlgebra (Lam (In t))          = "Lam (" <> prettyAlgebra t <> ")"
+  prettyAlgebra (Index i)             = "Var " <> (pack . show $ i)
+  prettyAlgebra (App (In t1) (In t2)) = "App (" <> prettyAlgebra t1
+                                      <> ") (" <> prettyAlgebra t2 <> ")"
 
 
 --------------------------------------------------------------------------------
 --                                PROGRAMS                                    --
 --------------------------------------------------------------------------------
 
-foo0 :: Fix Natural
-foo0 = In Zero     
+foo0 :: Mu Natural
+foo0 = In Zero
+
+foo1 :: Mu Lambda
+foo1 = In (Lam (In (Index 0)))
